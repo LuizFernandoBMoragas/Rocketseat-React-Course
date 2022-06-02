449dfa790df34f67e59680462c3c3b8e1698bc64
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-06-03 00:51:45 +0100

    adding default styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Post } from "./post";
-import "./app.css";
+import "./global.css";
+import { Header } from "./components/header";
 
 export function App() {
   return (
     <div className="App">
+      <Header />
       <Post
         author="Luiz Fernando"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui assumenda sequi, saepe repudiandae praesentium expedita perspiciatis iure at repellat a atque! Veritatis nostrum, fugiat repudiandae quasi expedita a deserunt excepturi."
