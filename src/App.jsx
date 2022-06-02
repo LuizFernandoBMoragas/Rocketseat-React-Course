85bc4a878f1e0e7a62225e09252ccb401e10f5e4
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-06-03 00:16:19 +0100

    creating first components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-function App() {
+import { Post } from "./post";
+import "./app.css";
+
+export function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Post
+        author="Luiz Fernando"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui assumenda sequi, saepe repudiandae praesentium expedita perspiciatis iure at repellat a atque! Veritatis nostrum, fugiat repudiandae quasi expedita a deserunt excepturi."
+      />
+      <Post author="Emili Maia" content="Amor da minha vida todinho" />
     </div>
   );
 }
-
-export default App;
