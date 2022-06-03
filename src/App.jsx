446497e83336a21136dc99cdf527916e44c3078b
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-06-03 01:45:10 +0100

    header component has been implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import { Post } from "./post";
-import "./global.css";
 import { Header } from "./components/header";
+
+import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
     <div className="App">
       <Header />
-      <Post
-        author="Luiz Fernando"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui assumenda sequi, saepe repudiandae praesentium expedita perspiciatis iure at repellat a atque! Veritatis nostrum, fugiat repudiandae quasi expedita a deserunt excepturi."
-      />
-      <Post author="Emili Maia" content="Amor da minha vida todinho" />
+      <div className={styles.wrapper}>
+        <aside>Sidebar</aside>
+        <main>
+          <Post
+            author="Luiz Fernando"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui assumenda sequi, saepe repudiandae praesentium expedita perspiciatis iure at repellat a atque! Veritatis nostrum, fugiat repudiandae quasi expedita a deserunt excepturi."
+          />
+          <Post
+            author="Emili Maia"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui assumenda sequi, saepe repudiandae praesentium expedita perspiciatis iure at repellat a atque! Veritatis nostrum, fugiat repudiandae quasi expedita a deserunt excepturi."
+          />
+        </main>
+      </div>
     </div>
   );
 }
