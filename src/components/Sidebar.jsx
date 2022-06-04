3ce3f9313410a2bee00befb3011a7a8283cdf028
Luiz Fernando Barros Moragas <luiz.moragas@hotmail.com>
2022-06-04 22:52:27 +0100

    sidebar has been completed

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -20,7 +21,10 @@ export function Sidebar() {
       </div>
 
       <footer>
-        <a href="#">Edit profile</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Edit profile
+        </a>
       </footer>
     </aside>
   );
